8e43514550143eec7125a66539d08af6de3077ed
mycp11 <john.cabanes@mnmarts.com>
2024-08-29 19:24:11 +0800

    code challenge

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Input Prime Number: ");
+string input = Console.ReadLine();
+
+//check prime numbers
+if(Convert.ToInt32(input) % 2 != 0) // even numbers
+{
+    Console.WriteLine("Prime number");
+}
+else
+{
+    Console.WriteLine("Even number");
+}
 
 
+
+Console.WriteLine("Input Palindrome: ");
+string palindromInput = Console.ReadLine();
+
+char[] reverseChar = palindromInput.ToCharArray();
+Array.Reverse(reverseChar);
+//string reverseInput = Convert.ToString(reverseChar);
+
+string reverseInput = new string(reverseChar);
+
+if (palindromInput == reverseInput)
+{
+    Console.WriteLine("Palindrome");
+}
+else
+{
+    Console.WriteLine("Not Palindrome");
+}
